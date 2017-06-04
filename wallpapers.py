974419ef322490e34a11e63bb1289f780d67e220
Ted Tang <s23tang@gmail.com>
2017-06-04 18:03:33 -0400

    changes

--- wallpapers.py
+++ wallpapers.py
@@ -4,6 +4,7 @@ import yaml
 import sys
 from bs4 import BeautifulSoup
 from time import sleep
+from subprocess import call
 import curses, os
 
 CONFIG_LOCATION = os.path.expanduser('~/.wpconfig')
@@ -37,11 +38,15 @@ n = curses.A_NORMAL #n is the coloring for a non highlighted menu option
 
 MENU = "menu"
 COMMAND = "command"
-EXIT_POS = -1
-PREV_PAGE = -2
-NEXT_PAGE = -3
+ST_EXIT = 'exit'
+ST_PREV = 'prev'
+ST_NEXT = 'next'
+ST_DEL = 'delete'
+ST_EXEC = 'exec'
 DOMAIN = 'https://interfacelift.com'
 SAVED_TEXT = ' (saved)'
+DOWNLOAD_CHUNK = 512*1024
+INITIAL_PAGE = 1
 
 def get_command(title, command):
     return { 'title': title, 'type': COMMAND , 'command': command }
@@ -61,12 +66,21 @@ def get_parsed_wps(url):
     wp_containers = soup.find_all('div', id=re.compile('^list_'))
     return map(parse_container, wp_containers)
 
-def download(url, filepath):
+def download(url, filepath, menu, getin, curr_page):
     with open(filepath, 'wb') as handle:
         res = requests.get(url, stream=True)
-        for block in res.iter_content(1024):
+        i = 0
+        total_length = int(res.headers.get('content-length'))
+        for block in res.iter_content(DOWNLOAD_CHUNK):
             if not block:
                 break
+            text = "{text} ({percent:.0f}%)".format(
+                text=get_item_text(menu, getin, curr_page),
+                percent=i*1.0*DOWNLOAD_CHUNK/total_length*100
+            )
+            screen.addstr(getin+9,4, text, n)
+            screen.refresh()
+            i += 1
             handle.write(block)
 
 def generate_parsed_command(parsed_item):
@@ -78,85 +92,91 @@ def get_menu_data(page):
     parsed = get_parsed_wps('https://interfacelift.com/wallpaper/downloads/date/widescreen/{}/index{}.html'.format(config[RESOLUTION], page))
     return get_menu('Download Wallpaper', 'Wallpapers', map(generate_parsed_command, parsed))
 
+def get_item_text(menu, index, page):
+    return "{number} - {title}".format(
+        number = (page-1) * len(menu['options']) + index + 1,
+        title = menu['options'][index]['title']
+    )
+
 # This function displays the appropriate menu and returns the option selected
 def runmenu(menu, parent, pos, page):
-  optioncount = len(menu['options']) # how many options in this menu
+    optioncount = len(menu['options']) # how many options in this menu
 
-  oldpos=None # used to prevent the screen being redrawn every time
-  x = None #control for while loop, let's you scroll through options until return key is pressed then returns pos to program
+    oldpos = None
+    status = None
+    x = None
 
-  # Loop until return key is pressed
-  while x !=ord('\n'):
-    if pos != oldpos:
-      oldpos = pos
-      screen.border(0)
-      screen.addstr(2,2, menu['title'], curses.A_STANDOUT) # Title for this menu
-      screen.addstr(4,2, 'Resolution: \'{}\', Saving in: \'{}\''.format(config[RESOLUTION], config[WP_DIR]), curses.A_BOLD)
-      screen.addstr(6,2, 'Page {} - {}'.format(page, menu['subtitle']), curses.A_BOLD) #Subtitle for this menu
+    # Loop until return key is pressed
+    while True:
+        if pos != oldpos:
+            oldpos = pos
+            screen.border(0)
+            screen.addstr(2,2, menu['title'], curses.A_STANDOUT)
+            screen.addstr(4,2, 'Resolution: \'{}\', Saving in: \'{}\''.format(config[RESOLUTION], config[WP_DIR]), curses.A_BOLD)
+            screen.addstr(6, 2, 'Controls: \'UP DOWN\' change rows, \'LEFT RIGHT\' change pages, \'ENTER\' to save/delete, \'O\' to open saved')
+            screen.addstr(8,2, 'Page {} - {}'.format(page, menu['subtitle']), curses.A_BOLD)
 
-      # Display all the menu items, showing the 'pos' item highlighted
-      for index in range(optioncount):
-        textstyle = h if pos == index else n
-        text = "{number} - {title}".format(
-            number = (page-1) * optioncount + index + 1,
-            title = menu['options'][index]['title']
-        )
-        screen.addstr(7+index,4, text, textstyle)
-        screen.clrtoeol()
-      screen.refresh()
+            # Display all the menu items, showing the 'pos' item highlighted
+            for index in range(optioncount):
+                textstyle = h if pos == index else n
+                screen.addstr(9+index,4, get_item_text(menu, index, page), textstyle)
+                screen.clrtoeol()
+                screen.refresh()
 
-    x = screen.getch() # Gets user input
+        x = screen.getch() # Gets user input
 
-    # What is user input?
-    if x == 113 or x == 27: # q or esc pressed
-      pos = EXIT_POS
-      break
-    elif x == 258: # down arrow
-      pos = (pos + 1) % optioncount
-    elif x == 259: # up arrow
-      pos =  (pos - 1) % optioncount
-    elif x == 260: # left arrow
-      pos = PREV_PAGE
-      break
-    elif x == 261: # right arrow
-      pos = NEXT_PAGE
-      break
+        # What is user input?
+        if x == 258: # down arrow
+            pos = (pos + 1) % optioncount
+        elif x == 259: # up arrow
+            pos =  (pos - 1) % optioncount
+        elif x == 260: # left arrow
+            status = ST_PREV
+            break
+        elif x == 261: # right arrow
+            status = ST_NEXT
+            break
+        elif x == ord('\n'):
+            status = ST_EXEC
+            break
+        elif x == ord('q') or x == 27: # q or esc pressed
+            status = ST_EXIT
+            break
+        elif x == 127:
+            status = ST_DEL
+            break
 
-  # return index of the selected item
-  return pos
-
-INITIAL_PAGE = 1
+    return pos, status
 
 # This function calls showmenu and then acts on the selected item
 def processmenu(menu, parent=None):
-  optioncount = len(menu['options'])
-  exitmenu = False
-  getin = 0
-  curr_page = INITIAL_PAGE
-  while not exitmenu: #Loop until the user exits the menu
-    getin = runmenu(menu, parent, getin, curr_page)
-    if getin == EXIT_POS:
-        exitmenu = True
-    elif getin == PREV_PAGE:
-        if curr_page > INITIAL_PAGE:
-            curr_page -= 1
+    optioncount = len(menu['options'])
+    getin = 0
+    curr_page = INITIAL_PAGE
+    while True:
+        getin, status = runmenu(menu, parent, getin, curr_page)
+        if status == ST_EXIT:
+            break
+        elif status == ST_PREV:
+            if curr_page > INITIAL_PAGE:
+                curr_page -= 1
+                menu = get_menu_data(curr_page)
+        elif status == ST_NEXT:
+            curr_page += 1
             menu = get_menu_data(curr_page)
-    elif getin == NEXT_PAGE:
-        curr_page += 1
-        menu = get_menu_data(curr_page)
-    elif menu['options'][getin]['type'] == COMMAND:
-      dl_url = menu['options'][getin]['url']
-      dl_path = menu['options'][getin]['dl_path']
-      if menu['options'][getin]['title'].endswith(SAVED_TEXT):
-        os.remove(dl_path)
-        menu['options'][getin]['title'] = menu['options'][getin]['title'][:-len(SAVED_TEXT)]
-      else:
-        download(dl_url, dl_path)
-        menu['options'][getin]['title'] += SAVED_TEXT
-    elif menu['options'][getin]['type'] == MENU:
-          screen.clear() #clears previous screen on key press and updates display based on pos
-          processmenu(menu['options'][getin], menu) # display the submenu
-          screen.clear() #clears previous screen on key press and updates display based on pos
+        elif status == ST_EXEC:
+            dl_url = menu['options'][getin]['url']
+            dl_path = menu['options'][getin]['dl_path']
+            if menu['options'][getin]['title'].endswith(SAVED_TEXT):
+                call("open -a Preview {}".format(dl_path).split(' '))
+            else:
+                download(dl_url, dl_path, menu, getin, curr_page)
+                menu['options'][getin]['title'] += SAVED_TEXT
+        elif status == ST_DEL:
+            dl_path = menu['options'][getin]['dl_path']
+            if menu['options'][getin]['title'].endswith(SAVED_TEXT):
+                os.remove(dl_path)
+                menu['options'][getin]['title'] = menu['options'][getin]['title'][:-len(SAVED_TEXT)]
 
 # Main program
 processmenu(get_menu_data(INITIAL_PAGE))
